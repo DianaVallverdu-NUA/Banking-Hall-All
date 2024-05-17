ae2608b0ab0732cd27897b74a922abd3baeb15fa
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2024-05-17 15:53:40 +0100

    arrows working

--- shared/messaging.js
+++ shared/messaging.js
@@ -1,61 +1,5 @@
 const secondsToDisplayMessage = 5;
 const div = document.getElementById("infoDiv");
-const forwardArrow = document.getElementById("forwardArrowButton");
-const backArrow = document.getElementById("backArrowButton");
-const pages = ["a", "b", "c"];
-let pageIndex;
-
-function getInitialIndex() {
-  const href = window.location.href;
-  const currentPage = href[href.length - 2];
-
-  switch (currentPage) {
-    case "a": {
-      pageIndex = 0;
-      return;
-    }
-    case "b": {
-      pageIndex = 1;
-      return;
-    }
-    case "c": {
-      pageIndex = 2;
-      return;
-    }
-  }
-}
-
-forwardArrow.onclick = () => {
-  //update page index
-  pageIndex++;
-  if (pageIndex >= pages.length) pageIndex = 0;
-
-  //get current page
-  const currentPage = window.location.href;
-
-  //update page value
-  const newPage =
-    currentPage.substring(0, currentPage.length - 2) + pages[pageIndex] + "/";
-
-  //navigate
-  window.location.href = newPage;
-};
-
-backArrow.onclick = () => {
-  //update page index
-  pageIndex--;
-  if (pageIndex < 0) pageIndex = pages.length - 1;
-
-  //get current page
-  const currentPage = window.location.href;
-
-  //update page value
-  const newPage =
-    currentPage.substring(0, currentPage.length - 2) + pages[pageIndex] + "/";
-
-  //navigate
-  window.location.href = newPage;
-};
 
 function displayMessage() {
   div.classList.remove("dissolve-out");
@@ -73,5 +17,4 @@ function intervalFunction() {
   secondsSinceInteraction++;
 }
 
-getInitialIndex();
 displayMessage();
---
+++ shared/navigation.js
@@ -0,0 +1,58 @@
+const forwardArrow = document.getElementById("forwardArrowButton");
+const backArrow = document.getElementById("backArrowButton");
+const pages = ["a", "b", "c"];
+let pageIndex;
+
+function getInitialIndex() {
+  const href = window.location.href;
+  const currentPage = href[href.length - 2];
+
+  switch (currentPage) {
+    case "a": {
+      pageIndex = 0;
+      return;
+    }
+    case "b": {
+      pageIndex = 1;
+      return;
+    }
+    case "c": {
+      pageIndex = 2;
+      return;
+    }
+  }
+}
+
+forwardArrow.onclick = () => {
+  //update page index
+  pageIndex++;
+  if (pageIndex >= pages.length) pageIndex = 0;
+
+  //get current page
+  const currentPage = window.location.href;
+
+  //update page value
+  const newPage =
+    currentPage.substring(0, currentPage.length - 2) + pages[pageIndex] + "/";
+
+  //navigate
+  window.location.href = newPage;
+};
+
+backArrow.onclick = () => {
+  //update page index
+  pageIndex--;
+  if (pageIndex < 0) pageIndex = pages.length - 1;
+
+  //get current page
+  const currentPage = window.location.href;
+
+  //update page value
+  const newPage =
+    currentPage.substring(0, currentPage.length - 2) + pages[pageIndex] + "/";
+
+  //navigate
+  window.location.href = newPage;
+};
+
+getInitialIndex();
