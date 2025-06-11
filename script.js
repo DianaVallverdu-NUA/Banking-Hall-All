f1620ac68d35959834f1ca3c554469b742eadec4
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-06-11 13:08:33 +0100

    set a as default

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const playButton = document.getElementById('playIcon');
 playButton.onclick = () => {
   
   const href = window.location.href;
-  window.location.href = href + 'b';
+  window.location.href = href + 'a';
 }
